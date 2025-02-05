5c7bd3fdeae5ba6b689df4e0e8add9b684562e2f
muhammed <ajferaju9961@gmail.com>
2025-02-05 14:36:05 +0530

    fix: final fix for axios

--- frontend/src/Utils/axios.ts
+++ frontend/src/Utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.BACKEND_API,
+  baseURL: "https://api-inkspire.mhdajfer.in/api",
   withCredentials: true,
 });
 
